0657739897a0c4a67b9bdfd68e780c498348fb85
Aggelos Biboudis <biboudis@gmail.com>
2013-10-07 17:54:38 +0300

    Fixed method invk. More testing to follow.

--- delphj-tests/A.java
+++ delphj-tests/A.java
@@ -1,14 +1,14 @@
 
 public class A {
 
-	public String meth1(String param1, Integer param2) {
+    public String meth1(String param1, Integer param2) {
 
-		System.out.println(param1);
+	System.out.println(param1);
 
-		return meth2("this is an arg to method2", 25);
-	}
+	return this.meth2("this is an arg to method2", 25);
+    }
 
-	public String meth2(String msg, Integer i) {
-		return "class A::meth2";
-	}
+    public String meth2(String msg, Integer i) {
+	return "class A::meth2";
+    }
 }
