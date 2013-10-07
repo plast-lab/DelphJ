edc3f6a7a67c8325db11abbcc080629ff5f87082
Aggelos Biboudis <biboudis@gmail.com>
2013-10-07 13:26:21 +0300

    Some Formatting.

--- delphj-tests/DandC.java
+++ delphj-tests/DandC.java
@@ -3,8 +3,20 @@ public class D {
     public subobject Integer five = new Integer(5);
     public subobject Integer six = new Integer(6);
 
+    Integer encode(String in) {
+	
+    }
+    
     void meth (){
 	Integer val = five;
 	
     }
 } 
+
+public class C {
+    public subobject D d;
+
+    Integer encode(String in) {
+	
+    }
+}
