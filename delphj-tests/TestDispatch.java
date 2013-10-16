49ede3c0acbb2a10d6983751c6a468b5dcddb05d
Aggelos Biboudis <biboudis@gmail.com>
2013-10-16 22:37:16 +0300

    Added debug print, categorized rewritings, splitted field accesses to cover two cases: 1) path merging with 4.2 and 2) invocation with 4.3.

--- delphj-tests/TestDispatch.java
+++ delphj-tests/TestDispatch.java
@@ -2,29 +2,24 @@
 class TestDispatch {
 
     public static void main(String[] args) {
-		A a = new A();
+	A a = new A();
 
-		B b = new B();
+	B b = new B();
 
-		C c = new C();
+	C c = new C();
 
-		//b.b1 = a;
-		b.b1 = a; 
+	b.b1 = a;
+	
+	// b.b1.compareTo(5);
 
-		//c.getData().c1.getData().b1 = a;
-		
-		//C tmp1 = (C) (c.getData());
-		//B tmp2 = (B) (tmp1.c1.getData());
-		//A tmp3 = (A) (tmp2.b1);
-		//a = tmp3;
-		//c.c1.b1 = a; 
+	// c.c1.b1 = a; 
 
-		//String result = b.b1.meth1("lala", 10);
+	// String result = b.b1.meth1("lala", 10);
 
-		// System.out.println("Result is " + result);
+	// System.out.println("Result is " + result);
 
-		// result = c.meth1(b);
+	// result = c.meth1(b);
 
-		// System.out.println("Result is " + result);
+	// System.out.println("Result is " + result);
     }
 }
