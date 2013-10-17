0d5fdee56382709662862af5f6561923bf83d3dc
Aggelos Biboudis <biboudis@gmail.com>
2013-10-17 17:15:30 +0300

    Testing subobject accesses with two paths c.c1. c.c1.b1 is still pending. Needs resolution of Package Access successively.

--- delphj-tests/TestDispatch.java
+++ delphj-tests/TestDispatch.java
@@ -8,11 +8,11 @@ class TestDispatch {
 
 	C c = new C();
 
-	c.c1.b1 = a;
-	
-	// b.b1.compareTo(5);
+	c.c1 = b;
 
-	// c.c1.b1 = a; 
+	// TODO: c.c1.b1 = a;
+	
+	// TODO: b.b1.compareTo(5);
 
 	// String result = b.b1.meth1("lala", 10);
 
